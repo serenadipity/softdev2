e4c1f4b7050e37e6133820350ca5b14f09739df5
serenadipity <serenagchan@gmail.com>
2016-02-12 11:58:21 -0500

    stuff

--- hw2/dotconn.js
+++ hw2/dotconn.js
@@ -1,8 +1,8 @@
 /* 
-Serena Chan
-SoftDev2 Pd 6
-HW#2 -- Connect the Dots
-2016-2-10
+   Serena Chan
+   SoftDev2 Pd 6
+   HW#2 -- Connect the Dots
+   2016-2-10
 */
 
 var c = document.getElementById("playground");
@@ -12,49 +12,30 @@ ctx.fillStyle= "#000066";
 ctx.strokeStyle = "#000000";
 ctx.strokeRect(10,10,520,520);
 
-/*
-var dots = function dots(e) {
-    ctx.beginPath();
-    ctx.moveTo(e.offsetX, e.offsetY);
-    ctx.arc(e.offsetX,e.offsetY,10,0,2*Math.PI);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
-    ctx.fill();
-    ctx.closePath();
-}
-*/
-
 var prevX;
 var prevY;
 
 var dots = function dots(e) {
-	var x = e.offsetX;
-	var y = e.offsetY;
-	ctx.beginPath();
-	ctx.moveTo(x,y);
-	ctx.arc(x,y,10,0,2*Math.PI);
-	ctx.fill();
-	if (prevX != null) {
-		ctx.lineTo(prevX,prevY);
-	}
-	ctx.stroke();
-	//ctx.fill();
-	prevX = x;
-	prevY = y;
-	ctx.closePath();
+    var x = e.offsetX;
+    var y = e.offsetY;
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    ctx.arc(x,y,10,0,2*Math.PI);
+    ctx.fill();
+    if (prevX != null) {
+	ctx.lineTo(prevX,prevY);
+	
+    }
+    ctx.stroke();
+    prevX = x;
+    prevY = y;
+    ctx.closePath();
 }
-
-/*
-var clear = function clear(e) {
-    e.preventDefault();
-    ctx.clearRect(0,0,538,538);
-}
-*/
 
 clear.addEventListener("click", function(e){
-	e.preventDefault();
-	ctx.clearRect(10,10,520,520);
-	prevX = null;
+    e.preventDefault();
+    ctx.clearRect(10,10,520,520);
+    prevX = null;
 })
 
 c.addEventListener("click",function(e) {
