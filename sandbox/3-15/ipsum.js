718778865ef3ebba08b32d5d7033881a5aedb018
serenadipity <serenagchan@gmail.com>
2016-03-16 11:31:32 -0400

    hipster

--- sandbox/3-15/ipsum.js
+++ sandbox/3-15/ipsum.js
@@ -1,1 +1,11 @@
-d3.selectAll("p").style("color", "blue");
+d3.selectAll("h1").style("color","white");
+//d3.selectAll("p").style("color", "white");
+
+d3.selectAll("p")
+    .data([16, 18, 20, 22])
+    .style("font-size", function(d) { return d + "px"; })
+
+d3.selectAll("p").style("color", function() {
+  return "hsl(" + Math.random() * 360 + ",100%,50%)";
+});
+d3.select("body").style("background-color", "black");
