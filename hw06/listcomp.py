5b952dce1548087ccea5a74d207a7080dcf248bc
serenadipity <serenagchan@gmail.com>
2016-03-31 11:34:06 -0400

    stuff

---
+++ hw06/listcomp.py
@@ -0,0 +1,6 @@
+def passtest(str)
+    #s = sum(str[])
+    UC_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    p = [1 for x in str if x in UC_LETTERS elif 2 for x in str x>='a' and
+         x<='z'elif 3 for x in strx>=0 and x<=9 else 0 for x in str]
+    return p
