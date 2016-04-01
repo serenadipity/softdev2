d1c34c94990cd2f8c85f636143ef75eb51547f9b
serenadipity <serenagchan@gmail.com>
2016-04-01 00:01:08 -0400

    stuff

--- hw06/listcomp.py
+++ hw06/listcomp.py
@@ -1,6 +1,11 @@
-def passtest(str)
-    #s = sum(str[])
+def passtest(str):
     UC_LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    p = [1 for x in str if x in UC_LETTERS elif 2 for x in str x>='a' and
-         x<='z'elif 3 for x in strx>=0 and x<=9 else 0 for x in str]
-    return p
+    LC_LETTERS="abcdefghijklmnopqrstuvwxyz"
+    nums = "1234567890"
+    symbols = ".?!&#,;:-_*"
+    p = [1 if x in UC_LETTERS else 2 if x in LC_LETTERS else 3 if x in nums
+    else 4 if x in symbols else 0 for x in str]
+    if 1 in p and 2 in p and 3 in p and 4 in p:
+    	return "strong"
+    else:
+    	return "doesn't meet requirements"
